977df2ed35376a7aa1d3041e53282867c4606cfc
Mihai <mcosma@gmail.com>
2022-12-28 08:16:00 +0000

    fix top mem by switching to /statm (#2)

--- plugins/dool_top_mem.py
+++ plugins/dool_top_mem.py
@@ -1,5 +1,6 @@
 ### Authority: Dag Wieers <dag$wieers,com>
 
+
 class dstat_plugin(dstat):
     """
     Most expensive CPU process.
@@ -8,38 +9,49 @@ class dstat_plugin(dstat):
     interval. The value displayed is the percentage of CPU time for the total
     amount of CPU processing power. Based on per process CPU information.
     """
+
     def __init__(self):
-        self.name = 'most expensive'
-        self.vars = ('memory process',)
-        self.type = 's'
+        self.name = "most expensive"
+        self.vars = ("memory process",)
+        self.type = "s"
         self.width = 17
         self.scale = 0
 
     def extract(self):
-        self.val['max'] = 0.0
+        self.val["max"] = 0.0
         for pid in proc_pidlist():
             try:
                 ### Using dopen() will cause too many open files
-                l = proc_splitline('/proc/%s/stat' % pid)
+                l = proc_splitline("/proc/%s/statm" % pid)
             except IOError:
                 continue
 
-            if len(l) < 23: continue
-            usage = int(l[23]) * pagesize
+            if len(l) < 1:
+                continue
+            usage = int(l[1]) * pagesize
 
             ### Is it a new topper ?
-            if usage <= self.val['max']: continue
+            if usage <= self.val["max"]:
+                continue
 
-            self.val['max'] = usage
-            self.val['name'] = getnamebypid(pid, l[1][1:-1])
-            self.val['pid'] = pid
+            self.val["max"] = usage
+            self.val["name"] = getnamebypid(
+                pid, proc_splitline("/proc/%s/comm" % pid)[0:-1]
+            )
+            self.val["pid"] = pid
 
-        self.output = '%-*s%s' % (self.width-5, self.val['name'][0:self.width-5], cprint(self.val['max'], 'f', 5, 1024))
+        self.output = "%-*s%s" % (
+            self.width - 5,
+            self.val["name"][0 : self.width - 5],
+            cprint(self.val["max"], "f", 5, 1024),
+        )
 
         ### Debug (show PID)
-#       self.val['memory process'] = '%*s %-*s' % (5, self.val['pid'], self.width-6, self.val['name'])
+
+    #       self.val['memory process'] = '%*s %-*s' % (5, self.val['pid'], self.width-6, self.val['name'])
 
     def showcsv(self):
-        return '%s / %d%%' % (self.val['name'], self.val['max'])
+        return "%s / %d%%" % (self.val["name"], self.val["max"])
+
 
 # vim:ts=4:sw=4:et
