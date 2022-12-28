a398a03d1cbb7f01cefb172c119fb88d0cd40653
Mihai <mcosma@gmail.com>
2022-12-28 01:40:38 -0500

    cleanup and add errorchecking

--- plugins/dool_blockchain.py
+++ plugins/dool_blockchain.py
@@ -1,4 +1,5 @@
 ### Author: Dag Wieers <dag$wieers,com>
+global requests
 import requests
 
 
@@ -15,25 +16,33 @@ class dstat_plugin(dstat):
         self.scale = 1
 
     def get_blocknumber(self, url):
-        block_in_hash = requests.post(
-            url=url,
-            json={"jsonrpc": "2.0", "method": "eth_blockNumber", "params": [], "id": 1},
-            timeout=5,
-        ).json()["result"]
+        try:
+            block_in_hash = requests.post(
+                url=url,
+                json={
+                    "jsonrpc": "2.0",
+                    "method": "eth_blockNumber",
+                    "params": [],
+                    "id": 1,
+                },
+                timeout=5,
+            ).json()["result"]
+        except Exception:
+            return 0
         blocknumber = int(block_in_hash, 16)
         return blocknumber
 
     def extract(self):
-
         mine = self.get_blocknumber(url="http://localhost:8545")
         llama = self.get_blocknumber(url="https://eth.llamarpc.com")
         mine -= round(mine / 1e6) * 1e6
         llama -= round(llama / 1e6) * 1e6
-        diff = llama - mine
 
-        self.set2["mine"] = float(mine)
-        self.set2["llama"] = float(llama)
-        self.set2["diff"] = float(llama - mine)
+        if mine > 0:
+            self.set2["mine"] = float(mine)
+        if llama > 0:
+            self.set2["llama"] = float(llama)
+        self.set2["diff"] = float(self.set2["llama"] - self.set2["mine"])
 
         self.val = self.set2
 
